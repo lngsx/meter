86c6fe6f2231ecc2a60965878001d2f27af3feab
LC <1572632+lngsx@users.noreply.github.com>
2025-12-19 20:41:33 +0700

    Add more claude model to the pricing table

--- src/config/pricing_table.rs
+++ src/config/pricing_table.rs
@@ -39,4 +39,10 @@ pub static PRICING: &[PricingTable] = &[
         input_multiplier: 6.0,
         output_multiplier: 22.5,
     },
+    PricingTable {
+        base_model_name: "claude-opus-4-5",
+        context_window: "0-200k", // Claude doesn't have long context pricing for this model.
+        input_multiplier: 5.0,
+        output_multiplier: 25.0,
+    },
 ];
